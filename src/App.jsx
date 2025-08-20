62dbc32ce4711ac6eccaab64526d488cd251f26c
Gaurav <124897570+gaurav9689@users.noreply.github.com>
2025-08-20 12:36:47 +0530

    update the code and same chenges

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import React, { Suspense, useState } from 'react'
 import './App.css'
-// import Cart from './components/Cart'
-// import CartItem from './components/CartItem'
-// import Header from './components/Header'
-// import NotFound from './components/NotFound'
-// import ProductDetail from './components/ProductDetail'
-// import ProductList from './components/ProductList'
-// import ProductItem from './components/ProductItem'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-// import Checkout from './components/Checkout'
 
 const Cart = React.lazy(() => import('./components/Cart'));
-// const Header = React.lazy(() => import('./components/Header'));
 const NotFound = React.lazy(() => import('./components/NotFound'));
 const ProductDetail = React.lazy(() => import('./components/ProductDetail'));
 const ProductList = React.lazy(() => import('./components/ProductList'));
 const Checkout = React.lazy(() => import('./components/Checkout'));
 
 function App() {
-  // const [count, setCount] = useState(0)
-
 
   return (
 
